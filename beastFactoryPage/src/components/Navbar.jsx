c52723b9522af6ebbf239d6ac8952794f80cfd12
unknown <kuba.majka.pl@gmail.com>
2024-12-28 14:42:05 +0100

    functional navbar

--- beastFactoryPage/src/components/Navbar.jsx
+++ beastFactoryPage/src/components/Navbar.jsx
@@ -14,60 +14,60 @@ const Navbar = () => {
   };
 
   return (
-        <div className="navBar" >
-          <div className="Logo">
-            <NavLink to="/">Logo</NavLink>
-          </div>
+    <div className="navBar" >
+      <div className="Logo">
+        <NavLink to="/">Logo</NavLink>
+      </div>
 
-          <div className="pages">
-            <div
-              className="dropdown"
-              onMouseEnter={() => handleMouseEnter("weights")}
-              onMouseLeave={handleMouseLeave}
-            >
-              <NavLink to="/weights">Ciężary</NavLink>
-              {activeDropdown === "weights" && (
-                <div className="dropdownMenu">
-                  <NavLink to="/weights/1">ciezary 1</NavLink>
-                  <NavLink to="/weights/2">ciezary 2</NavLink>
-                </div>
-              )}
+      <div className="pages">
+        <div
+          className="dropdown"
+          onMouseEnter={() => handleMouseEnter("weights")}
+          onMouseLeave={handleMouseLeave}
+        >
+          <NavLink to="/weights">Ciężary</NavLink>
+          {activeDropdown === "weights" && (
+            <div className="dropdownMenu">
+              <NavLink to="/weights/1">ciezary 1</NavLink>
+              <NavLink to="/weights/2">ciezary 2</NavLink>
             </div>
-            <div
-              className="dropdown"
-              onMouseEnter={() => handleMouseEnter("machines")}
-              onMouseLeave={handleMouseLeave}
-            >
-              <NavLink to="/machines">Maszyny</NavLink>
-              {activeDropdown === "machines" && (
-                <div className="dropdownMenu">
-                  <NavLink to="/machines/1">maszyny 1</NavLink>
-                  <NavLink to="/machines/2">maszyny 2</NavLink>
-                </div>
-              )}
-            </div>
-            <div
-              className="dropdown"
-              onMouseEnter={() => handleMouseEnter("bundles")}
-              
-            >
-              <NavLink to="/bundles">Zestawy</NavLink>
-              {activeDropdown === "bundles" && (
-                <div className="dropdownMenu" onMouseLeave={handleMouseLeave}>
-                  <NavLink to="/bundles/1">Zestawy 1</NavLink>
-                  <NavLink to="/bundles/2">Zestawy 2</NavLink>
-                </div>
-              )}
-            </div>
-          </div>
-
-
-          <div className="user">
-            <NavLink to="/Search">Szukaj</NavLink>
-            <NavLink to="/Account">Konto</NavLink>
-            <NavLink to="/Basket">Koszyk</NavLink>
-          </div>
+          )}
         </div>
+        <div
+          className="dropdown"
+          onMouseEnter={() => handleMouseEnter("machines")}
+          onMouseLeave={handleMouseLeave}
+        >
+          <NavLink to="/machines">Maszyny</NavLink>
+          {activeDropdown === "machines" && (
+            <div className="dropdownMenu">
+              <NavLink to="/machines/1">maszyny 1</NavLink>
+              <NavLink to="/machines/2">maszyny 2</NavLink>
+            </div>
+          )}
+        </div>
+        <div
+          className="dropdown"
+          onMouseEnter={() => handleMouseEnter("bundles")}
+          
+        >
+          <NavLink to="/bundles">Zestawy</NavLink>
+          {activeDropdown === "bundles" && (
+            <div className="dropdownMenu" onMouseLeave={handleMouseLeave}>
+              <NavLink to="/bundles/1">Zestawy 1</NavLink>
+              <NavLink to="/bundles/2">Zestawy 2</NavLink>
+            </div>
+          )}
+        </div>
+      </div>
+
+
+      <div className="user">
+        <NavLink to="/Search">Szukaj</NavLink>
+        <NavLink to="/Account">Konto</NavLink>
+        <NavLink to="/Basket">Koszyk</NavLink>
+      </div>
+    </div>
   );
 };
 
